67b48df30ddbf06d42242185a0d742684a8395ab
MATHEUS DE ALMEIDA CANTARUTTI <matheus.acantarutti@senacsp.edu.br>
2025-11-13 20:57:55 -0300

    get and setters for employees

--- hrPointMarking/src/main/java/br/com/cantarutti/hrPointMarking/model/Employee.java
+++ hrPointMarking/src/main/java/br/com/cantarutti/hrPointMarking/model/Employee.java
@@ -29,5 +29,61 @@ public class Employee {
         this.LevelRole = levelRole;
         this.Role = role;
     };
+
+    public String getRegistration() {
+        return this.Registration;
+    }
+
+    public void setRegistration(String Registration) {
+        this.Registration = Registration;
+    }
+
+    public String getName() {
+        return this.Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getAdmissionDate() {
+        return this.AdmissionDate;
+    }
+
+    public void setAdmissionDate(String AdmissionDate) {
+        this.AdmissionDate = AdmissionDate;
+    }
+
+    public String getDepartament() {
+        return this.Departament;
+    }
+
+    public void setDepartament(String Departament) {
+        this.Departament = Departament;
+    }
+
+    public String getLevel() {
+        return this.Level;
+    }
+
+    public void setLevel(String Level) {
+        this.Level = Level;
+    }
+
+    public Integer getLevelRole() {
+        return this.LevelRole;
+    }
+
+    public void setLevelRole(Integer LevelRole) {
+        this.LevelRole = LevelRole;
+    }
+
+    public String getRole() {
+        return this.Role;
+    }
+
+    public void setRole(String Role) {
+        this.Role = Role;
+    }
     
 }
