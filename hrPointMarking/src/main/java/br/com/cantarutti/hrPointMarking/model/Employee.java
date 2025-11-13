b11759d3fdafeedd5d7a2ee464087a313d6b30eb
MATHEUS DE ALMEIDA CANTARUTTI <matheus.acantarutti@senacsp.edu.br>
2025-11-13 20:29:32 -0300

    employee constructor

--- hrPointMarking/src/main/java/br/com/cantarutti/hrPointMarking/model/Employee.java
+++ hrPointMarking/src/main/java/br/com/cantarutti/hrPointMarking/model/Employee.java
@@ -2,4 +2,32 @@ package br.com.cantarutti.hrPointMarking.model;
 
 public class Employee {
     
+    private String Registration;
+    private String Name;
+    private String AdmissionDate;
+    private String Departament;
+    private String Level;
+    private Integer LevelRole;
+    private String Role;
+
+    public Employee() {};
+
+    public Employee(
+        String registration, 
+        String name, 
+        String adminissionDate,
+        String departement,
+        String level,
+        Integer levelRole,
+        String role
+    ) {
+        this.Registration = registration;
+        this.Name = name;
+        this.AdmissionDate = adminissionDate;
+        this.Departament = departement;
+        this.Level = level;
+        this.LevelRole = levelRole;
+        this.Role = role;
+    };
+    
 }
